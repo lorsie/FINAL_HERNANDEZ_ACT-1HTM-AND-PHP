22d0b7da04b768d07adcef587f5f484e44ea144b
lorsie <152486611+lorsie@users.noreply.github.com>
2024-10-01 17:05:27 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+
+---NUMBER 1---
+
+<?php
+session_start();
+$message = '';
+
+$alreadyLoggedInUser = '';
+
+// Handle login
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
+    $username = htmlspecialchars($_POST['username']);
+    $password = htmlspecialchars($_POST['password']);
+
+    // Check if the user is already logged in
+    if (isset($_SESSION['username'])) {
+        $message = $_SESSION['username'] . " is already logged in. Please log out first.";
+    } else {
+        if ($username === $alreadyLoggedInUser) {
+            $message = "$username is already logged in. Wait for him to log out first.";
+        } else {
+            if (empty($username) || empty($password)) {
+                $message = "Both fields are required!";
+            } else {  
+                $_SESSION['username'] = $username;
+                $message = "<h3>User Logged in: $username</h3>";
+            }
+        }
+    }
+}
+
+// Handle logout
+if (isset($_POST['logout'])) {
+    session_unset();
+    session_destroy();
+    $message = "You have been logged out!";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+    <link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+    <form method="POST" action="">
+        <label for="username">Username: <input type="text" id="username" name="username"></label><br><br>
+        <label for="password">Password: <input type="password" id="password" name="password"></label><br><br>
+        <input type="submit" name="login" value="Login" class="button"><br><br>
+        <input type="submit" name="logout" value="Logout" class="button">
+    </form>
+
+    <p><?php echo $message; ?></p>
+</body>
+</html>
